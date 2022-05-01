e79b619133854b839ac00102582e4a256ac2a144
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-05-01 14:56:25 -0500

    update weather route with public request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const needle = require('needle');
 const apicache = require('apicache');
-
+// Init cache
+let cache = apicache.middleware;
 // Env vars
 const {
     API_BASE_URL,
@@ -12,8 +13,28 @@ const {
     CACHE_TIME
 } = process.env;
 
-// Init cache
-let cache = apicache.middleware;
+router.get('/', cache(`${CACHE_TIME}`), async (req, res) => {
+    try {
 
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            ...url.parse(req.url, true).query
+        })
+
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const data = apiRes.body
+
+        // Log the public API request
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+        }
+
+        res.status(200).json(data)
+
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+
+})
 
 module.exports = router;
