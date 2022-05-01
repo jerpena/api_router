eb3104af47dc19fb10477ceaf90ec56317e12484
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-05-01 14:46:19 -0500

    initialize basic express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit')
+require('dotenv').config();
+const {
+    PORT = 5000,
+    RATE_LIMIT_TIME = 3e+5,
+    MAX_RATE = 5
+} = process.env
+
+const app = express();
+
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
